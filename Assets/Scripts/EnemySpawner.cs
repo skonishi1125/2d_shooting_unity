69a97a17c1f96a1321788ecee48198b467aab327
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-18 00:49:33 +0800

    スポーン処理の実装

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,16 +1,47 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Camera cam;
+
+    [Header("Spawn Information")]
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnInterval = .3f;
+    private float spawnTimer = 0f;
+    [SerializeField] private float viewportSpawnX;
+    [SerializeField] private float viewportSpawnY;
+    [SerializeField] private Vector2 viewportSpawnPosition;
+    [SerializeField] private Vector2 worldSpawnPosition;
+
+
+
+    private void Start()
     {
-        
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0f)
+        {
+            Spawn();
+            spawnTimer = spawnInterval;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Spawn()
     {
-        
+        if (enemyPrefab == null)
+            return;
+
+        // x軸はSpawnerのposition, y軸は画面のy軸内でランダムに沸かせる
+        viewportSpawnX = cam.WorldToViewportPoint(gameObject.transform.position).x;
+        viewportSpawnY = Random.Range(0f, 1.0f);
+
+        viewportSpawnPosition = new Vector2(viewportSpawnX, viewportSpawnY);
+        worldSpawnPosition = cam.ViewportToWorldPoint(viewportSpawnPosition);
+
+
+        Instantiate(enemyPrefab, worldSpawnPosition, Quaternion.identity);
     }
 }
