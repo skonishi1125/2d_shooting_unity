ad4526ea1d39a724c9c26e572c4c19b79ed699b2
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-19 10:53:38 +0800

    ScriptableObjectで、WaveDataを管理できるようにした

---
+++ Assets/Scripts/WaveData.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Shooting Stage Wave Data")]
+public class WaveData : ScriptableObject
+{
+    [System.Serializable]
+    public class SpawnEvent
+    {
+        public float time;
+        public EnemyBase enemyPrefab;
+        [Range(0f, 1f)]
+        public float viewportY;
+    }
+
+    public List<SpawnEvent> events;
+
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private WaveData waveData;
+    private float timer = 0f;
+    private int nextIndex = 0;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        // waveDataに設定したSpawnが尽きたら終了
+        if (nextIndex >= waveData.events.Count)
+            return;
+
+        var e = waveData.events[nextIndex];
+
+        if(timer > e.time)
+        {
+            SpawnEnemy(e);
+            nextIndex++;
+        }
+
+    }
+
+    private void SpawnEnemy(WaveData.SpawnEvent e)
+    {
+
+        // x軸はSpawnerのpositionを使う
+        var viewportSpawnX = cam.WorldToViewportPoint(gameObject.transform.position).x;
+        var viewportSpawnY = e.viewportY;
+
+        var viewportSpawnPosition = new Vector3(viewportSpawnX, viewportSpawnY, 0f);
+        var worldSpawnPosition = cam.ViewportToWorldPoint(viewportSpawnPosition);
+        worldSpawnPosition.z = 0f;
+
+        Instantiate(e.enemyPrefab, worldSpawnPosition, Quaternion.identity);
+
+    }
+}
