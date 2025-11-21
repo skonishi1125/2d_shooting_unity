f8d7acee2c70b032a5359de07db940584f75a2ed
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-11-21 12:21:51 +0800

    被弾時にフラッシュするようにした

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -2,17 +2,54 @@
 
 public class EnemyHealth : MonoBehaviour
 {
+    protected SpriteRenderer sr;
+
     private float hitPoint;
     [SerializeField] private float maxHitPoint;
+
+    [Header("Hit Flash")]
+    [SerializeField] private Color baseColor;
+    [SerializeField] private Color hitColor = Color.white;
+    [SerializeField] private float flashDuration = 0.05f;
+    private float flashTimer = 0f;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     private void Start()
     {
         hitPoint = maxHitPoint;
+        baseColor = sr.color;
     }
+
+    private void Update()
+    {
+        UpdateHitFlash();
+    }
+
+    // 演出が増えてきた場合、
+    // EnemyHitFlashというコンポーネントを作って切り出しても良い
+    private void UpdateHitFlash()
+    {
+        if (flashTimer <= 0f)
+            return;
+
+        flashTimer -= Time.deltaTime;
+        if (flashTimer <= 0f)
+        {
+            sr.color = baseColor;
+        }
+    }
+
 
     public void TakeDamage(float damage)
     {
         hitPoint = hitPoint - damage;
+        sr.color = hitColor;
+        flashTimer = flashDuration;
+
         if (hitPoint <= 0f)
             Die();
     }
